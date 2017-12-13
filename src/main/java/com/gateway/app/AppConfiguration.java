1492ac5de33d2b900682642bf4ca933f7251be22
Ellen Heitman <ellen_heitman@mastercard.com>
2017-12-13 11:05:07 -0600

    Remove temp debugging statements

--- src/main/java/com/gateway/app/AppConfiguration.java
+++ src/main/java/com/gateway/app/AppConfiguration.java
@@ -9,10 +9,6 @@ public class AppConfiguration {
     @Bean
     public Config buildConfig() {
         //TODO: Check if these system props exists, otherwise terminate the process
-        System.out.println("################### GATEWAY_MERCHANT_ID" + System.getenv("GATEWAY_MERCHANT_ID"));
-        System.out.println("################### GATEWAY_API_PASSWORD" + System.getenv("GATEWAY_API_PASSWORD"));
-        System.out.println("################### GATEWAY_BASE_URL" + System.getenv("GATEWAY_BASE_URL"));
-        System.out.println("################### GATEWAY_API_VERSION" + System.getenv("GATEWAY_API_VERSION"));
         return new Config(System.getenv("GATEWAY_MERCHANT_ID"), System.getenv("GATEWAY_API_PASSWORD"), System.getenv("GATEWAY_BASE_URL"), Integer.valueOf(System.getenv("GATEWAY_API_VERSION")));
     }
 }
