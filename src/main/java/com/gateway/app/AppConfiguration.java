2d46feda787e3548a4543a9855c49704cfcc41c6
Siddique Hameed <siddique_hameed@mastercard.com>
2017-12-14 13:22:08 -0600

    Remove comment

--- src/main/java/com/gateway/app/AppConfiguration.java
+++ src/main/java/com/gateway/app/AppConfiguration.java
@@ -8,7 +8,6 @@ public class AppConfiguration {
 
     @Bean
     public Config buildConfig() {
-        //TODO: Check if these system props exists, otherwise terminate the process
         return new Config(System.getenv("GATEWAY_MERCHANT_ID"), System.getenv("GATEWAY_API_PASSWORD"), System.getenv("GATEWAY_BASE_URL"), Integer.valueOf(System.getenv("GATEWAY_API_VERSION")));
     }
 }
