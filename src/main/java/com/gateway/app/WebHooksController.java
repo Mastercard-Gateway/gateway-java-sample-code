b27a693029cc83b8c57cdfa09c81b00d9b8eb6f6
Siddique Hameed <siddique_hameed@mastercard.com>
2017-12-15 16:20:47 -0600

    List Webhook Notifications

--- src/main/java/com/gateway/app/WebHooksController.java
+++ src/main/java/com/gateway/app/WebHooksController.java
@@ -5,9 +5,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.File;
@@ -28,7 +26,7 @@ public class WebHooksController {
     }
 
     @GetMapping("/list-webhook-notifications")
-    public List<WebhookNotification> listWebhooks() throws IOException {
+    public @ResponseBody List<WebhookNotification> listWebhooks() throws IOException {
         System.out.println("Listing Webhooks Notifications...");
 
         File notificationsFolder = new File(Config.WEBHOOKS_NOTIFICATION_FOLDER);
