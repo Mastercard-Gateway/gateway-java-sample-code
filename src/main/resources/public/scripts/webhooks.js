91db17deb5eb2bbc2f040f65915b14753bdf5d25
Siddique Hameed <siddique_hameed@mastercard.com>
2017-12-18 09:50:31 -0600

    Display webhook notifications

--- src/main/resources/public/scripts/webhooks.js
+++ src/main/resources/public/scripts/webhooks.js
@@ -5,10 +5,9 @@ $(function () {
         $.each(data, function (key, val) {
             notifications.push("<tr><th scope=\"row\">" + val.timestamp + "</th><td>" + val.orderId + "</td><td>" + val.transactionId + "</td><td>" + val.orderStatus + "</td><td>" + val.amount + "</td></tr>");
         });
-
-        console.log("##### Notifications = ", notifications);
+        console.log("Notifications = ", notifications);
         if (notifications.length == 0) {
-            var notificationUrl = window.location.href.substring(0, window.location.href.lastIndexOf('/'));
+            var notificationUrl = window.location.href.substring(0, window.location.href.lastIndexOf('/')) + "/process-webhook";
             var noNotificationText = "No notifications found, please configure the url - '" + notificationUrl + "' in merchant settings to receive notifications.";
 
             $('.no-notification').append(noNotificationText).removeClass('invisible');
