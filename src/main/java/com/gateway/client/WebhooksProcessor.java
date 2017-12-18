6e14f104620dd3944e5f4170abf0d9de56588fbd
Siddique Hameed <siddique_hameed@mastercard.com>
2017-12-18 09:57:08 -0600

    Display webhook notifications

---
+++ src/main/java/com/gateway/client/WebhooksProcessor.java
@@ -0,0 +1,7 @@
+package com.gateway.client;
+
+/**
+ * Created by siddique on 12/18/17.
+ */
+public class WebhooksProcessor {
+}
--- src/main/resources/public/scripts/webhooks.js
+++ src/main/resources/public/scripts/webhooks.js
@@ -3,13 +3,12 @@ $(function () {
     $.getJSON("list-webhook-notifications", function (data) {
         var notifications = [];
         $.each(data, function (key, val) {
-            notifications.push("<tr><th scope=\"row\">" + val.timestamp + "</th><td>" + val.orderId + "</td><td>" + val.transactionId + "</td><td>" + val.orderStatus + "</td><td>" + val.amount + "</td></tr>");
+            notifications.push("<tr><td scope=\"row\">" + new Date(val.timestamp) + "</td><td>" + val.orderId + "</td><td>" + val.transactionId + "</td><td>" + val.orderStatus + "</td><td>" + val.amount + "</td></tr>");
         });
         console.log("Notifications = ", notifications);
         if (notifications.length == 0) {
             var notificationUrl = window.location.href.substring(0, window.location.href.lastIndexOf('/')) + "/process-webhook";
             var noNotificationText = "No notifications found, please configure the url - '" + notificationUrl + "' in merchant settings to receive notifications.";
-
             $('.no-notification').append(noNotificationText).removeClass('invisible');
         }
         else {
