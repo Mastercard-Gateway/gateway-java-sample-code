3be7ea2203823d222bece5eb8ff89e207f83a887
Siddique Hameed <siddii@gmail.com>
2018-12-03 08:31:53 -0600

    Add runServer.sh

--- runServer.sh
+++ runServer.sh
@@ -1,7 +1,7 @@
 
 # Update this file for setting up Merchant Id and other environmental configurations for the test server
 
-set GATEWAY_MERCHANT_ID=YOUR_MERCHANT_ID
-set GATEWAY_API_PASSWORD=YOUR_API_PASSWORD
-set GATEWAY_BASE_URL=YOUR_GATEWAY_BASE_URL
+export GATEWAY_MERCHANT_ID=YOUR_MERCHANT_ID
+export GATEWAY_API_PASSWORD=YOUR_API_PASSWORD
+export GATEWAY_BASE_URL=YOUR_GATEWAY_BASE_URL
 java -jar dist/gateway-java-sample-code-1.0.jar
