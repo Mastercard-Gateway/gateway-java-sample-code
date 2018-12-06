c67849681e51173c69a3a3efdad75d114922db52
Jorge Arenas <jorshx@gmail.com>
2018-12-06 16:29:38 -0600

    Added proxy support

--- runServer.sh
+++ runServer.sh
@@ -4,4 +4,4 @@
 export GATEWAY_MERCHANT_ID=YOUR_MERCHANT_ID
 export GATEWAY_API_PASSWORD=YOUR_API_PASSWORD
 export GATEWAY_BASE_URL=YOUR_GATEWAY_BASE_URL
-java -jar dist/gateway-java-sample-code-1.0.jar
+java -Dhttp.proxyHost=YOUR_PROXY_HOST -Dhttp.proxyPort=YOUR_PROXY_URL -jar dist/gateway-java-sample-code-1.0.jar
