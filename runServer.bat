e887839848fb734c384f18920dbfd8ac5512e4a8
Jorge Arenas <jorge.arenas@mastercard.com>
2019-02-06 14:01:41 -0600

    Adding runServer configurations and jar file

---
+++ runServer.bat
@@ -0,0 +1,6 @@
+# Update this file for setting up Merchant Id and other environmental configurations for the test server
+
+set GATEWAY_MERCHANT_ID=YOUR_MERCHANT_ID
+set GATEWAY_API_PASSWORD=YOUR_API_PASSWORD
+set GATEWAY_BASE_URL=YOUR_GATEWAY_BASE_URL
+java -jar dist/gateway-java-sample-code-1.0.jar
---
+++ runServer.sh
@@ -0,0 +1,6 @@
+# Update this file for setting up Merchant Id and other environmental configurations for the test server
+
+export GATEWAY_MERCHANT_ID=YOUR_MERCHANT_ID
+export GATEWAY_API_PASSWORD=YOUR_API_PASSWORD
+export GATEWAY_BASE_URL=YOUR_GATEWAY_BASE_URL
+java -jar dist/gateway-java-sample-code-1.0.jar
