91b201f1a8643f58af55bcacf4babe25b970a1e3
Jorge Arenas <jorge.arenas@mastercard.com>
2019-02-11 12:02:18 -0600

    Updated .jar version

--- runServer.bat
+++ runServer.bat
@@ -3,4 +3,4 @@
 set GATEWAY_MERCHANT_ID=YOUR_MERCHANT_ID
 set GATEWAY_API_PASSWORD=YOUR_API_PASSWORD
 set GATEWAY_BASE_URL=YOUR_GATEWAY_BASE_URL
-java -jar dist/gateway-java-sample-code-1.0.jar
+java -jar dist/gateway-java-sample-code-1.1.0.jar
--- runServer.sh
+++ runServer.sh
@@ -3,4 +3,4 @@
 export GATEWAY_MERCHANT_ID=YOUR_MERCHANT_ID
 export GATEWAY_API_PASSWORD=YOUR_API_PASSWORD
 export GATEWAY_BASE_URL=YOUR_GATEWAY_BASE_URL
-java -jar dist/gateway-java-sample-code-1.0.jar
+java -jar dist/gateway-java-sample-code-1.1.0.jar
