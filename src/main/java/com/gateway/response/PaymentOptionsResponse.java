b92ee749c0f53c99a74874002c4946f52e996a66
Miriam Lee <miriam.lee@mastercard.com>
2019-03-19 16:09:48 -0600

    Transaction Mode in Config page

--- src/main/java/com/gateway/response/PaymentOptionsResponse.java
+++ src/main/java/com/gateway/response/PaymentOptionsResponse.java
@@ -4,6 +4,8 @@
 
 package com.gateway.response;
 
+import com.gateway.model.TransactionMode;
+
 /**
  * The options available for processing a payment, for example, the credit cards and currencies.
  */
@@ -22,23 +24,6 @@ public final class PaymentOptionsResponse {
     public PaymentOptionsResponse setTransactionMode(TransactionMode transactionMode) {
         this.transactionMode = transactionMode;
         return this;
-    }
-
-    /**
-     * Defines the merchants transaction mode, i.e. if the funds are immediately requested to be moved from the payer's
-     * account or the payment is authorized only and the funds will be moved/captured later.
-     */
-    public enum TransactionMode {
-        /**
-         * The payment is authorized only (AUTHORIZATION transaction). The merchant must submit a Capture request to
-         * move the funds from the payer's account to the merchant's account.
-         */
-        AUTHORIZE_CAPTURE,
-        /**
-         * The payment happens immediately (PURCHASE transaction). The funds are immediately moved from the payer's
-         * account to the merchant's account.
-         */
-        PURCHASE
     }
 
 }
